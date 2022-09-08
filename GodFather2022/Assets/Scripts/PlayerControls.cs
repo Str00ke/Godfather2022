543e7b4d39551f98f81dc0cf13f292ec49511fb8
Str0ke_ <charlelie.garrigues@gmail.com>
2022-09-08 20:00:27 +0200

    Fix

--- GodFather2022/Assets/Scripts/PlayerControls.cs
+++ GodFather2022/Assets/Scripts/PlayerControls.cs
@@ -38,8 +38,10 @@ public class PlayerControls : MonoBehaviour
     void Update()
     {
         // Movements
-
-        Vector2 movements = new Vector2(Input.GetAxisRaw("Horizontal"), playerRb.velocity.y);
+        float h = 0;
+        if (Input.GetKey(KeyCode.Q)) h = -1;
+        else if (Input.GetKey(KeyCode.D)) h = 1;
+        Vector2 movements = new Vector2(/*Input.GetAxisRaw("Horizontal")*/h, playerRb.velocity.y);
 
         if (movements.x < 0 && baseVec.x == 0) movements.x = 0;
         if (movements.x > 0 && baseVec.y == 0) movements.x = 0;
@@ -48,7 +50,7 @@ public class PlayerControls : MonoBehaviour
 
         //Jump
 
-        if (Input.GetKeyDown(KeyCode.Z) && baseVec.z > 0)
+        if (Input.GetKeyDown(KeyCode.Space) && baseVec.z > 0)
         {
             if (isGrounded)
             {
