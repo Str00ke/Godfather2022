da0d128164b621f44ff11838bf7582623502914c
Str0ke_ <charlelie.garrigues@gmail.com>
2022-09-09 10:00:07 +0200

    Fix stop menu music

--- GodFather2022/Assets/Scripts/PlayerControls.cs
+++ GodFather2022/Assets/Scripts/PlayerControls.cs
@@ -37,6 +37,7 @@ public class PlayerControls : MonoBehaviour
     void Start()
     {
         FindObjectOfType<AudioManager>().Play("BgMusic");
+        FindObjectOfType<AudioManager>().Stop("MenuMusic");
 
         animator = GetComponent<Animator>();
     }
